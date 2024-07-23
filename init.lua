b02c49d3cf6581e38d4f267fb8d08aba6302a067
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-07-23 02:20:46 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/bestexecutorscript/main/scalein.lua", true))() --this one laggy so its special
 files = {
+  "iconspam.lua"
   "blurify.lua",
   "camerakiller.lua",
   "circles.lua",
@@ -16,6 +18,7 @@ for _, l in files do
   spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/bestexecutorscript/main/"..l, true))()
   end)
-  wait(0.5)
+  wait(20)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/bestexecutorscript/main/"..l, true))()
 end
 print("LOVE FROM CRYSTALHUB!")
