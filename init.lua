d8d91e6ca51e2559d5eade81a05c61d037f5a25f
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-07-23 01:55:16 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,19 @@
+files = {
+  "blurify.lua",
+  "camerakiller.lua",
+  "circles.lua",
+  "fovdestoryer.lua",
+  "invert.lua",
+  "notifications.lua",
+  "paint.lua",
+  "popups.lua",
+  "reversetext.lua",
+  "screenoverload.lua",
+  "screenswipers.lua",
+}
 
+for _, l in files do
+  loadstring(game:HttpGet(l, true))()
+  wait(1)
+end
+print("LOVE FROM CRYSTALHUB!")
