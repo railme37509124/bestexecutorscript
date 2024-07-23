d7cf0a09c69bf768a74cb43b728480be9646815a
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-07-23 01:50:30 +0100

    Update reversetext.lua

--- reversetext.lua
+++ reversetext.lua
@@ -1,5 +1,13 @@
-for i, v in game:GetDescendants() do
-  if v:IsA("TextLabel") or v:IsA("TextButton") then
-    v.Text = string.reverse(v.Text)
+function rev()
+  for i, v in game:GetDescendants() do
+    if v:IsA("TextLabel") or v:IsA("TextButton") then
+      v.Text = string.reverse(v.Text)
+    end
   end
 end
+
+spawn(function()
+  while task.wait(2)
+    rev()
+  end
+end)
